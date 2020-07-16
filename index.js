bf4050a05f0a024493d4e68533575be5ca1096a6
deepak.bisht <deepak.bisht@oracle.com>
2020-07-16 09:18:55 +0530

    refactor index.js and include winston for logging.

--- index.js
+++ index.js
@@ -1,42 +1,12 @@
 const winston = require('winston');
-const config = require('config');
-const error = require('./middleware/error');
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
 const express = require('express');
-const mongoose = require('mongoose');
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
-const home = require('./routes/home');
 const app = express();
 
-winston.add(winston.transports.File, { filename : 'logfile.log' });
-
-if (!config.get('jwtPrivateKey')) {
-    console.error('FATAL: jwtPrivateKey is not defined');
-    process.exit(1);
-}
-mongoose.connect('mongodb://localhost/vidly', { useNewUrlParser: true,  useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Could not connect to mongoDB', err));
-
-app.set('view engine', 'pug');
-app.set('views', './views');
-
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-app.use('/', home);
-
-app.use(error);
+require('./startup/logging');
+require('./startup/route')(app);
+require('./startup/db')();
+require('./startup/config')();
+require('./startup/validation')();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(port, () => winston.info(`Listening on port ${port}...`));
